3892d6670194da9abda1c43bc73d25d1cda937d6
KatherineYuneman <kyuneman@gmail.com>
2021-10-18 13:28:17 -0400

    commit tree fe7e695446dc7c6809e5c5aaabd18a9c5506b2ec

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+
+function upperCaseCustomerName(){
+   console.log(customerName.toUpperCase())
+};
+upperCaseCustomerName();
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+setBestCustomer();
